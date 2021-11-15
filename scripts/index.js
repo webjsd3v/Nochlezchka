2b1426f1b7cbe073166ba1ddbb29c9ab14a091e6
Ilya Avdeev <webjsd3v@yandex.ru>
2021-11-15 07:05:37 +0300

    Revert "Merge pull request #2 from webjsd3v/feature/events-section"
    This reverts commit b6feebc2a6faf96861b2a65081b750f78a1364db, reversing
    changes made to ad53b965a61cc6f714af40650b2945ebe981683f.

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-//Скроллинг card в секции events
-document
-  .querySelector('.events__scroll-area')
-  .addEventListener('wheel', function (event) {
-    if (event.deltaY != 0) {
-      this.scrollLeft += event.deltaY;
-      event.preventDefault();
-    }
-  });
